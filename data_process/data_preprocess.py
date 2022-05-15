3a0e0773fe1074a6181cb8abc78a66dfea2cd374
cai-jianfeng <1622613693@qq.com>
2022-05-15 23:27:10 +0800

    change get_label_list

--- data_process/data_preprocess.py
+++ data_process/data_preprocess.py
@@ -12,9 +12,15 @@ import xlsxwriter
 
 class Data:
     def __int__(self):
-        pass
+        self.data_sets = None
+        self.label_set = None
     
     def get_data_list(self, data_path):
+        """
+        获取指定path的数据
+        :param data_path: 数据路径 --> str
+        :return: 数据(三维:[channel, row, column]) --> numpy array
+        """
         sheets = xlrd.open_workbook(data_path)
         sheets_num = sheets.nsheets
         # sheets_names = sheets.sheet_names() # 获取所有sheet的名字
@@ -32,21 +38,47 @@ class Data:
                     data_set[row][column] = data
             data_sets.append(data_set)
         data_sets = np.array(data_sets)
-        # dim = data_sets.shape[0]
-        # SPAN = np.array([[0 for _ in range(columns)] for _ in range(rows)])
-        # for i in [0, 4, 8]:
-        #     SPAN = SPAN + data_sets[i]  # SPAN = T11 + T22 + T33
-        # print(data_sets.shape)  # 9 * 1400 * 1200
+        self.data_sets = data_sets
         return data_sets
     
-    def save_data_segmentation(self, data_path, patch_size):
+    def get_label_list(self, label_path):
         """
-        数据集切分（对一整张图像的数据进行切分成patch并保存）
-        :param data_path: 原始数据(整张图像)的路径
-        :param patch_size: 切分的数据块的大小([row, column])
+        获取指定路径的标签集
+        :param label_path: 标签的path --> str
+        :return: 标签集(二维数据:[row, column]) --> numpy array
+        """
+        sheets = xlrd.open_workbook(label_path)
+        sheet = sheets.sheet_by_index(0)
+        rows = sheet.nrows  # 获取sheet页的行数
+        columns = sheet.ncols  # 获取sheet页的列数
+        label_set = [[0 for _ in range(columns)] for _ in range(rows)]
+        for row in range(rows):
+            for column in range(columns):
+                # cell = sheet.cell(row, column)  # 获取单元格
+                # label = cell.value  # 获取单元格数据
+                label = sheet.cell_value(row, column)  # 获取单元格数据
+                label_set[row][column] = label
+        
+        label_set = np.array(label_set)
+        # print(label_set.shape)  # 1400 * 1200
+        self.label_set = label_set
+        return label_set
+
+    def save_data_label_segmentation(self, data_path, label_path, patch_size):
+        """
+        数据集切分（对一整张图像的数据进行切分成patch大小并保存）
+        :param data_path: 原始数据(整张图像)的路径 --> str
+        :param patch_size: 切分的数据块的大小([row, column]) --> tuple
         :return: None
         """
-        data_sets = self.get_data_list(data_path=data_path)  # 获取数据
+        if self.data_sets:  # 获取数据
+            data_sets = self.data_sets
+        else:
+            data_sets = self.get_data_list(data_path=data_path)
+        if self.label_set:  # 获取标签
+            label_set = self.label_set
+        else:
+            label_set = self.get_label_list(label_path=label_path)
         dim = data_sets.shape  # 数据维度:(channel, row, column)
         num = 0
         for channel in range(dim[0]):
@@ -59,19 +91,4 @@ class Data:
                             sheet.write(row, column, data_sets[channel][i + row][j + column])
                     book.close()
                     num += 1
-    
-    def get_label_list(self, label_path):
-        sheets = xlrd.open_workbook(label_path)
-        sheet = sheets.sheet_by_index(0)
-        rows = sheet.nrows  # 获取sheet页的行数
-        columns = sheet.ncols  # 获取sheet页的列数
-        label_set = [[0 for _ in range(columns)] for _ in range(rows)]
-        for row in range(rows):
-            for column in range(columns):
-                cell = sheet.cell(row, column)  # 获取单元格
-                label = cell.value  # 获取单元格数据
-                label_set[row][column] = label
-        
-        label_set = np.array(label_set)
-        # print(label_set.shape)  # 1400 * 1200
-        return label_set
+
